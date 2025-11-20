07f4aaf934306555f77a510a7798be2693580fcf
Tankó Ádám <tanko.adam@students.jedlik.eu>
2025-11-20 11:42:57 +0100

    terv izé

--- DataService.cs
+++ DataService.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Kocsma_raktár
 {
-    internal class DataService
+    public class DataService
     {
+        private readonly string stockPath = "Data/stock.json";
+        private readonly string categoryPath = "Data/categories.json";
+
+        public List<DrinkItem> LoadStock()
+        {
+            if (File.Exists(stockPath)) return new List<DrinkItem>();
+            return JsonSerializer.Deserialize<List<DrinkItem>>(File.ReadAllText(path: stockPath));
+        }
+
+        public void SaveStock(List<DrinkItem> stock)
+        {
+            File.WriteAllText(stockPath, JsonSerializer.Serialize(stock, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        public List<string> LoadCategories()
+        {
+            if (!File.Exists(categoryPath)) return new List<string>();
+            return JsonSerializer.Deserialize<List<string>>(File.ReadAllText(categoryPath));
+        }
     }
 }
--- DrinkItem.cs
+++ DrinkItem.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace Kocsma_raktár
 {
-    internal class DrinkItem
+    public class DrinkItem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int Quantity { get; set; }
+        public bool InStock { get; set; }
+        public DateTime LastRestock { get; set; }
     }
 }
